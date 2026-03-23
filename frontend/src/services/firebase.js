fde90f62c69c4f432e1d911f058f8084d7768008
Felipe Corrêa <correagfe@gmail.com>
2026-03-23 14:42:53 -0300

    update

--- frontend/src/services/firebase.js
+++ frontend/src/services/firebase.js
@@ -1,23 +1,42 @@
-// Import the functions you need from the SDKs you need
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from 'firebase/auth'
+// import { GoogleAuthProvider } from 'firebase/auth';
+
+
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD8gNW4z2zZDf2w6_UHQLRLbPvEJm-kfxg",
+//   authDomain: "journeymapeasy.firebaseapp.com",
+//   projectId: "journeymapeasy",
+//   storageBucket: "journeymapeasy.appspot.com",
+//   messagingSenderId: "741919285657",
+//   appId: "1:741919285657:web:526c48ccd7b6e8d3d8f308"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// export const auth = getAuth();
+
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth'
-import { GoogleAuthProvider } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD8gNW4z2zZDf2w6_UHQLRLbPvEJm-kfxg",
-  authDomain: "journeymapeasy.firebaseapp.com",
-  projectId: "journeymapeasy",
-  storageBucket: "journeymapeasy.appspot.com",
-  messagingSenderId: "741919285657",
-  appId: "1:741919285657:web:526c48ccd7b6e8d3d8f308"
+  apiKey: "AIzaSyCpk8RK6hG6SRM1fcb3Cnn7TdwTDjjeJsA",
+  authDomain: "journeymapeasy-1dbc6.firebaseapp.com",
+  projectId: "journeymapeasy-1dbc6",
+  storageBucket: "journeymapeasy-1dbc6.firebasestorage.app",
+  messagingSenderId: "159977605689",
+  appId: "1:159977605689:web:246f353883d87b80511be8",
+  measurementId: "G-NR5Z1WE6P7"
 };
 
-// Initialize Firebase
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth();
+// Inicializa e exporta a Autenticação (passando a variável 'app' dentro)
+export const auth = getAuth(app);
