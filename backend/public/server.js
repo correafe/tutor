718b4cba38a4c7adef069526d2919cbd94930fe4
Felipe Corrêa <correagfe@gmail.com>
2026-07-10 14:25:39 -0300

    update

--- backend/public/server.js
+++ backend/public/server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
+
+app.use(cors({
+    origin: 'https://gjem.vercel.app', // URL exata do seu frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}))
 
 app.use(express.json());
 
